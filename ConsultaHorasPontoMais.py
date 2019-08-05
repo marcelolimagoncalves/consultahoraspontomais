7a63bc196f0d0df91be2923b03471f2593eea0b5
Marcelo Gonçalves <marcelolimagoncalves@gmail.com>
2019-08-05 15:28:38 -0300

    Adaptando para criar uma imagem docker

--- ConsultaHorasPontoMais.py
+++ ConsultaHorasPontoMais.py
@@ -2,6 +2,8 @@
 import os, sys
 import smtplib
 import time
+import logging
+from pyvirtualdisplay import Display
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from email.mime.multipart import MIMEMultipart
@@ -13,23 +15,84 @@ from datetime import datetime
 ################ Configurações ##################
 
 # Configurações pontomais
+
+# Mínimo LogLevel
+log_level = 20 # Log Level
+try:
+    log_level = logging._nameToLevel[os.environ['LOG_LEVEL']]
+except:
+    pass
+logging.getLogger().setLevel(log_level)
+logging.info("log_level: " + str(log_level))
+
 pag_login_pontomais = "https://app.pontomaisweb.com.br/" # Página de Login do pontomais
+
 login_pontomais = "" # Login do pontomais
+try:
+    login_pontomais = os.environ['LOGIN_PONTOMAIS']
+except:
+    pass
+logging.info("login_pontomais: " + login_pontomais)
+
 senha_pontomais = "" # Senha do pontomais
+try:
+    senha_pontomais = os.environ['SENHA_PONTOMAIS']
+except:
+    pass
+logging.info("senha_pontomais: " + senha_pontomais)
 
 # Configurações do email
-msg = MIMEMultipart()
-msg['From'] = "" # Endereço de email
-msg['To'] = "" # Endereço de email
-msg['Subject'] = "PontoMais" # Assunto do email
+
+email_from = "limamarcelo2017@outlook.com" # Endereço de email
+try:
+    email_from = os.environ['LOGIN_EMAIL']
+except:
+    pass
+logging.info("email_from: " + email_from)
+
+email_to = "limamarcelo2017@outlook.com" # Endereço de email
+try:
+    email_to = os.environ['LOGIN_EMAIL']
+except:
+    pass
+logging.info("email_to: " + email_to)
+
+email_subject = "PontoMais" # Assunto do email
+try:
+    email_subject = os.environ['ASSUNTO_EMAIL']
+except:
+    pass
+logging.info("email_subject: " + email_subject)
+
 smtp_server = "SMTP.office365.com: 587" # Servidor SMPT para o envio do email. Outlook: SMTP.office365.com: 587 | Gmail: smtp.gmail.com: 587
-senha_email = "" # Senha do email
+try:
+    smtp_server = os.environ['SMPT_SERVER']
+except:
+    pass
+logging.info("smtp_server: " + smtp_server)
+
+email_senha = "Mar97908994" # Senha do email
+try:
+    email_senha = os.environ['SENHA_EMAIL']
+except:
+    pass
+logging.info("email_senha: " + email_senha)
 
 # Configuração do intervalo de tempo em minutos para cada verificação no site pontomais
-intervalo_verificacao = 5
+intervalo_verificacao = 10
+try:
+    intervalo_verificacao = int(os.environ['INTERVALO_VERIFICACAO'],2) 
+except:
+    pass
+logging.info("intervalo_verificacao: " + str(intervalo_verificacao))
 
 # Driver do navegador que deseja utilizar
 driver_padrao = "Firefox" # Valores: "Firefox", "Chrome"
+try:
+    driver_padrao = os.environ['DRIVER_PADRAO']
+except:
+    pass
+logging.info("driver_padrao: " + driver_padrao)
 
 #################################################
 
@@ -73,7 +136,14 @@ def buscar_registro_ponto():
         fechar_driver(driver)
         return registro_ponto
     except:
-        print("Erro ao buscar registro ponto")
+        logging.error("Erro ao buscar registro ponto")
+
+def buscar_registro_ponto_mock():
+    try:
+        registro_ponto = RegistroPonto("07:30","13:10","","")
+        return registro_ponto
+    except:
+        logging.error("Erro ao buscar registro ponto")
 
 def montar_mensagem_email(registro_ponto):
     try:
@@ -82,7 +152,7 @@ def montar_mensagem_email(registro_ponto):
                 "\nSegunda Entrada: " + registro_ponto.get_segunda_entrada() + \
                 "\nSegunda Saida: " + registro_ponto.get_segunda_saida()
     except:
-        print("Erro ao montar a mensagem de email")
+        logging.error("Erro ao montar a mensagem de email")
 
 def buscar_primeira_entrada(driver):
     try:
@@ -111,19 +181,61 @@ def buscar_segunda_saida(driver):
 def iniciar_driver():
     try:
         if driver_padrao == "Firefox":
-            return webdriver.Firefox()
+            display = Display(visible=0, size=(800, 600))
+            display.start()
+            logging.info('Initialized virtual display..')
+
+            firefox_profile = webdriver.FirefoxProfile()
+            firefox_profile.set_preference('browser.download.folderList', 2)
+            firefox_profile.set_preference('browser.download.manager.showWhenStarting', False)
+            firefox_profile.set_preference('browser.download.dir', os.getcwd())
+            firefox_profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'text/csv')
+
+            logging.info('Prepared firefox profile..')
+
+            browser = webdriver.Firefox(firefox_profile=firefox_profile)
+            logging.info('Initialized firefox browser..')
+            return browser
         elif driver_padrao == "Chrome":
-            return webdriver.Chrome()
+            display = Display(visible=0, size=(800, 600))
+            display.start()
+            logging.info('Initialized virtual display..')
+
+            chrome_options = webdriver.ChromeOptions()
+            chrome_options.add_argument('--no-sandbox')
+
+            chrome_options.add_experimental_option('prefs', {
+                'download.default_directory': os.getcwd(),
+                'download.prompt_for_download': False,
+            })
+            logging.info('Prepared chrome options..')
+
+            browser = webdriver.Chrome(chrome_options=chrome_options)
+            logging.info('Initialized chrome browser..')
         else:
-            return webdriver.Firefox()
+            display = Display(visible=0, size=(800, 600))
+            display.start()
+            logging.info('Initialized virtual display..')
+
+            firefox_profile = webdriver.FirefoxProfile()
+            firefox_profile.set_preference('browser.download.folderList', 2)
+            firefox_profile.set_preference('browser.download.manager.showWhenStarting', False)
+            firefox_profile.set_preference('browser.download.dir', os.getcwd())
+            firefox_profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'text/csv')
+
+            logging.info('Prepared firefox profile..')
+
+            browser = webdriver.Firefox(firefox_profile=firefox_profile)
+            logging.info('Initialized firefox browser..')
+            return browser
     except:
-        print("Erro ao iniciar o driver")
+        logging.error("Erro ao iniciar o driver")
 
 def fechar_driver(driver):
     try:
         driver.quit()
     except:
-        print("Erro ao fechar o driver")
+        logging.error("Erro ao fechar o driver")
 
 def entrar_pagina_historico(driver):
     try:
@@ -134,7 +246,7 @@ def entrar_pagina_historico(driver):
         driver.find_element(By.XPATH,"//a[contains(@href, '#/meu_ponto/" + hoje_formatado + "/historico')]").click()
         time.sleep(5)
     except:
-        print("Erro ao entrar na pagina de historico do pontomais")
+        logging.error("Erro ao entrar na pagina de historico do pontomais")
     
 def entrar_pagina_login(driver):
     try:
@@ -146,37 +258,68 @@ def entrar_pagina_login(driver):
         driver.find_element(By.CSS_SELECTOR, '.btn.btn-block.btn-bold.btn-primary.ng-binding').click()
         time.sleep(5)
     except:
-        print("Erro ao entrar na pagina de login")
+        logging.error("Erro ao entrar na pagina de login")
 
 def enviar_email(mensagem_email):
     try:
-        msg.attach(MIMEText(mensagem_email, 'plain'))
+        email_message = f'''{mensagem_email}'''
+        email_message = 'Subject: {}\n\n{}'.format(email_subject, email_message)
         server = smtplib.SMTP(smtp_server)
         server.starttls()
-        server.login(msg['From'], senha_email)
-        server.sendmail(msg['From'], msg['To'], msg.as_string())
+        server.login(email_from, email_senha)
+        server.sendmail(email_from, email_to, email_message)
         server.quit()
+        logging.info("Email enviado com sucesso")
     except:
-        print("Erro ao enviar email")
+        logging.error("Erro ao enviar email")
 
 def main():
-    print("Iniciando a execucao do programa" )
+    logging.info("Iniciando a execucao do programa")
 
-    primeira_saida_existe = False
-    while(not primeira_saida_existe):
+    data_primeira_entrada_enviada = datetime.min
+    data_primeira_saida_enviada = datetime.min
+    data_segunda_entrada_enviada = datetime.min
+    data_segunda_saida_enviada = datetime.min
 
-        registro_ponto = buscar_registro_ponto() 
+    while(True):
+        registro_ponto = buscar_registro_ponto()
+        hoje = datetime.today().strftime('%d-%m-%Y')
 
-        if registro_ponto.primeira_saida != "" and registro_ponto.primeira_saida != "Primeira Saida nao encontrada":
+        if data_primeira_entrada_enviada != hoje and registro_ponto.primeira_entrada != "" and registro_ponto.primeira_entrada != "Primeira Entrada nao encontrada":
             mensagem_email = montar_mensagem_email(registro_ponto)
             enviar_email(mensagem_email)
-            primeira_saida_existe = True
-            print("Primeira Saida encontrada" )
+            data_primeira_entrada_enviada = hoje
+            logging.info("Primeira Entrada encontrada")
         else:
-            print("Primeira Saida nao encontrada" )
-            time.sleep(intervalo_verificacao * 60)
+            logging.info("Primeira Entrada nao encontrada")
 
-    print("Encerrando a execucao do programa" )
+        if data_primeira_saida_enviada != hoje and registro_ponto.primeira_saida != "" and registro_ponto.primeira_saida != "Primeira Saida nao encontrada":
+            mensagem_email = montar_mensagem_email(registro_ponto)
+            enviar_email(mensagem_email)
+            data_primeira_saida_enviada = hoje
+            logging.info("Primeira Saida encontrada")
+        else:
+            logging.info("Primeira Saida nao encontrada")
+
+        if data_segunda_entrada_enviada != hoje and registro_ponto.segunda_entrada != "" and registro_ponto.segunda_entrada != "Segunda Entrada nao encontrada":
+            mensagem_email = montar_mensagem_email(registro_ponto)
+            enviar_email(mensagem_email)
+            data_segunda_entrada_enviada = hoje
+            logging.info("Segunda Entrada encontrada")
+        else:
+            logging.info("Segunda Entrada nao encontrada")
+            
+        if data_segunda_saida_enviada != hoje and registro_ponto.segunda_saida != "" and registro_ponto.segunda_saida != "Segunda Saida nao encontrada":
+            mensagem_email = montar_mensagem_email(registro_ponto)
+            enviar_email(mensagem_email)
+            data_segunda_saida_enviada = hoje
+            logging.info("Segunda Saida encontrada")
+        else:
+            logging.info("Segunda Saida nao encontrada")
+
+        time.sleep(intervalo_verificacao * 60)
+
+    logging.info("Encerrando a execucao do programa")
 
 if __name__ == "__main__":
     main()
